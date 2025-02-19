cc9f991b0a8a654832e1309ad2304607a9115cb5
YongsakSaengsuk <Yongsaksaengsuk@gmail.com>
2025-02-19 20:21:39 +0700

    test options

--- WebService/router/test.route.js
+++ WebService/router/test.route.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const { testSys } = require("../controller/test.controller.js");
+const express = require("express");
+const { testSys, testJWT , testgenToken} = require("../controller/test.controller.js");
 const route = express.Router();
+const authMiddleware = require("../Middleware/authMiddleware.js");
 
-route.get('/test', testSys);
-
+route.get("/test", testSys);
+route.get("/testAuth", authMiddleware, testJWT);
+route.get('/testgenToken', testgenToken);
 module.exports = route;
