0e67dede3cb2732c8fe13976bcbe209f8cbc1dd0
shape2003 <chevykittipon2546@gmail.com>
2025-02-20 16:13:54 +0700

    ทำหน้า create post เสร็จแล้ว..........มั้ง..........ครับ.............บอส

--- WebClient/src/main.ts
+++ WebClient/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { HttpClientModule } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent,{
+  providers: [HttpClientModule]
+})
   .catch((err) => console.error(err));
